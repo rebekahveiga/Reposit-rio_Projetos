0cc98718d42b6867193a54ef898592487236fb71
unknown <rebekahcaroline2002@gmail.com>
2023-02-26 03:25:56 -0300

    upload archives

---
+++ script.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper(".mySwiper",{cssMode:!0,loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"},mousewheel:!0,keyboard:!0});
